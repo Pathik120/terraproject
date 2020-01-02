5198e903a520c306231703c5601df084c60c46ef
Pathik <pathik119@gmail.com>
2020-01-02 12:12:22 +0000

    variables.tf

--- terra/variables.tf
+++ terra/variables.tf
@@ -13,12 +13,12 @@ variable "network_address_space" {
   default = "10.0.0.0/16"
 }
 variable "subnet1_address_space" {
-  default = "10.0.0.0/24"
-}
-variable "subnet2_address_space" {
   default = "10.1.0.0/24"
 }
+variable "subnet2_address_space" {
+  default = "10.1.1.0/24"
+}
 variable "subnet3_address_space" {
-  default = "10.0.1.0/24"
+  default = "10.1.2.0/24"
   description = "CIDR for the Private Subnet"
 }
