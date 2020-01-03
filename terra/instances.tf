bef984778472fe5b184b676f5c0d85953d742efc
Pathik <pathik119@gmail.com>
2020-01-03 06:03:52 +0000

    added provisioner in the aws backend type

--- terra/instances.tf
+++ terra/instances.tf
@@ -90,14 +90,7 @@ resource "aws_instance" "backend" {
       "sudo apt-get install python sshpass -y",
     ]
   }
-  
-
-}
-
-######################## Ansible setup ########################
-
-resource "null_resource" "ansible-main" {
-provisioner "local-exec" {
+  provisioner "local-exec" {
   command = <<EOT
         sleep 100;
         > jenkins-ci.ini;
@@ -107,4 +100,21 @@ provisioner "local-exec" {
         ansible-playbook -e  sshKey=${var.private_key_path} -i jenkins-ci.ini ./ansible/setup-backend.yaml -u ubuntu -v
     EOT
 }
+  
+
 }
+
+######################## Ansible setup ########################
+
+# resource "null_resource" "ansible-main" {
+# provisioner "local-exec" {
+#  command = <<EOT
+#       sleep 100;
+#        > jenkins-ci.ini;
+#        echo "[jenkins-ci]"| tee -a jenkins-ci.ini;
+#        export ANSIBLE_HOST_KEY_CHECKING=False;
+#        echo "${aws_instance.backend.public_ip}" | tee -a jenkins-ci.ini;
+#       ansible-playbook -e  sshKey=${var.private_key_path} -i jenkins-ci.ini ./ansible/setup-backend.yaml -u ubuntu -v
+#    EOT
+#}
+#}
