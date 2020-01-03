9f9866876db5715ce376b7cb4cadb356708baad1
Pathik <pathik119@gmail.com>
2020-01-03 08:44:17 +0000

    added the ansible part

--- terra/instances.tf
+++ terra/instances.tf
@@ -94,15 +94,15 @@ resource "aws_instance" "backend" {
 
 ######################## Ansible setup ########################
 
-# resource "null_resource" "ansible-main" {
-# provisioner "local-exec" {
-#  command = <<EOT
-#       sleep 100;
-#        > jenkins-ci.ini;
-#        echo "[jenkins-ci]"| tee -a jenkins-ci.ini;
-#        export ANSIBLE_HOST_KEY_CHECKING=False;
-#        echo "${aws_instance.backend.public_ip}" | tee -a jenkins-ci.ini;
-#       ansible-playbook -e  sshKey=${var.private_key_path} -i jenkins-ci.ini ./ansible/setup-backend.yaml -u ubuntu -v
-#    EOT
-#}
-#}
+ resource "null_resource" "ansible-main" {
+ provisioner "local-exec" {
+  command = <<EOT
+       sleep 100;
+        > jenkins-ci.ini;
+        echo "[jenkins-ci]"| tee -a jenkins-ci.ini;
+        export ANSIBLE_HOST_KEY_CHECKING=False;
+        echo "${aws_instance.backend.public_ip}" | tee -a jenkins-ci.ini;
+       ansible-playbook -e  sshKey=${var.private_key_path} -i jenkins-ci.ini ./ansible/setup-backend.yaml -u ubuntu -v
+    EOT
+}
+}
