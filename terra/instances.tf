21bd0f065256f60f107d435181ae9e8ade492615
Pathik <pathik119@gmail.com>
2020-01-05 04:42:00 +0000

    changes the ansible null resource

--- terra/instances.tf
+++ terra/instances.tf
@@ -67,6 +67,20 @@ resource "aws_instance" "nginx2" {
   }
 }
 
+ resource "null_resource" "ansible-main" {
+ provisioner "local-exec" {
+  command = <<EOT
+        sleep 100;
+        > jenkins-ci.ini;
+        echo "[jenkins-ci]"| tee -a jenkins-ci.ini;
+        export ANSIBLE_HOST_KEY_CHECKING=False;
+        echo "${aws_instance.backend.public_ip}" | tee -a jenkins-ci.ini;
+        ansible-playbook -e  sshKey=${var.private_key_path} -i jenkins-ci.ini ./ansible/setup-backend.yaml -u ubuntu -v
+    EOT
+}
+}
+
+
 ###################### INSTANCES backend ###################
 
 resource "aws_instance" "backend" {
@@ -92,17 +106,7 @@ resource "aws_instance" "backend" {
   }
 }
 
+
+
 ######################## Ansible setup ########################
 
- resource "null_resource" "ansible-main" {
- provisioner "local-exec" {
-  command = <<EOT
-       sleep 100;
-        > jenkins-ci.ini;
-        echo "[jenkins-ci]"| tee -a jenkins-ci.ini;
-        export ANSIBLE_HOST_KEY_CHECKING=False;
-        echo "${aws_instance.backend.public_ip}" | tee -a jenkins-ci.ini;
-        /usr/bin/ansible-playbook -e  sshKey=${var.private_key_path} -i jenkins-ci.ini ./ansible/setup-backend.yaml -u ubuntu -v
-    EOT
-}
-}
